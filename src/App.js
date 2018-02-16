61d37f15a24a478d2c2a0885f10235d0b03b05f3
Romulus <romulus.senter@yahoo.com>
2018-02-15 20:31:45 -0500

    created two buttons fot the planet and the people

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import axios from "axios";
 
 const BASE_URL = "HTTPS://swapi.co/api/";
 const PEOPLE_URL = `${BASE_URL}people`;
+const PLANET_URL = `${BASE_URL}planets`;
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       people: [],
+      planets: [],
       loading: true
     };
+    this.fetchPlanets = this.fetchPlanets.bind(this);
+    this.fetchPeople = this.fetchPeople.bind(this);
+    
   }
   componentDidMount() {
     console.log("component did mount");
@@ -20,15 +25,37 @@ class App extends Component {
       .then(response => this.setState({ loading: false,  people: response.data.results }))
       .catch(error => console.log(error));
   }
+  fetchPlanets(){
+    this.setState({loading:true});
+    axios.get(PLANET_URL)
+    .then(response => this.setState({loading: false,  planets: response.data.results }))
+      .catch(error => console.log(error));
+  }
+  fetchPeople(){
+    this.setState({loading: true});
+    axios
+    .get(PEOPLE_URL)
+    .then(response => this.setState({loading: false, people: response.data.results, planets: []}))
+    .catch(err => console.log(err));
+  }
+  
   render() {
     if(this.state.loading){
       return<h1>Loading.....</h1>
     }
-    const people = this.state.people.map(person => <li>{person.name}</li>);
+    const planets =this.state.planets.map((planets, index) => 
+      <li key={index}>{planets.name}</li>
+    );
+    const people = this.state.people.map((person, index) => <li key={index} >{person.name}</li>);
     return (
-      <ul>
-      {people}
-      </ul>
+      <div>
+      <ul>{planets.length === 0 ? people : planets}</ul>
+      {planets.length === 0 ?
+      <button onClick={this.fetchPlanets}>What are planets?</button>:
+    
+      <button onClick={this.fetchPeople}>What are the people?</button>}
+      
+      </div>
     );
   }}
 
